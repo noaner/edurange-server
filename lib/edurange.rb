1a639c2d46a5410ed3310a045b6402be87b9e216
Stefan Boesen <stefan.boesen@gmail.com>
2013-02-15 11:26:46 -0800

    Merged parser into gem

--- lib/edurange.rb
+++ lib/edurange.rb
@@ -5,22 +5,22 @@ require "edurange/edu_machine"
 
 module Edurange
   class Init
-    def self.init
+    def self.init(config_filename)
       keyname = "newkey"
-      ami_id = "ami-e720ad8e"
       our_ssh_key = Edurange::PuppetMaster.get_our_ssh_key()
       puppetmaster_ip = Edurange::PuppetMaster.puppetmaster_ip()
 
-      # do this each VM
-      certs = Edurange::PuppetMaster.gen_client_ssl_cert() 
-      conf = Edurange::PuppetMaster.generate_puppet_conf(certs[0])
+      nodes = Edurange::Parser.parse_yaml(config_filename) # format: nodes[node_name, ami_id, users, firewall_rules, packages]
 
-      facts = Edurange::Parser.facter_facts(certs[0])
-      Edurange::PuppetMaster.write_shell_config_file(our_ssh_key,puppetmaster_ip, certs, conf, facts)
-
-
-      machine = Edurange::EduMachine.new(certs[0], keyname, ami_id)
-      p machine.spin_up()
+      nodes.each do |node|
+        ami_id = node[0]
+        certs = Edurange::PuppetMaster.gen_client_ssl_cert() 
+        conf = Edurange::PuppetMaster.generate_puppet_conf(certs[0])
+        facts = Edurange::Parser.facter_facts(certs[0])
+        Edurange::PuppetMaster.write_shell_config_file(our_ssh_key,puppetmaster_ip, certs, conf, facts)
+        machine = Edurange::EduMachine.new(certs[0], keyname, ami_id)
+        p machine.spin_up()
+      end
     end
   end
 end
