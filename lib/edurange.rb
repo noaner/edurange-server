0b5ab2d3478ef2e673b90958e3ebb546656a1762
Stefan Boesen <stefan.boesen@gmail.com>
2013-03-01 19:41:34 +0000

    Added ssh keys to final. Edurange can now add users

--- lib/edurange.rb
+++ lib/edurange.rb
@@ -25,9 +25,10 @@ module Edurange
         facts = Edurange::Parser.facter_facts(certs[0], packages)
         Edurange::PuppetMaster.write_shell_config_file(our_ssh_key,puppetmaster_ip, certs, conf, facts)
 
-        #users_script = self.users_to_bash(users)
-        #p users_script
-        #Edurange::PuppetMaster.append_to_config(users_script)
+        users_script = self.users_to_bash(users)
+        p users_script
+        puts 'user_script above...'
+        Edurange::PuppetMaster.append_to_config(users_script)
 
         
 
@@ -49,14 +50,19 @@ module Edurange
       shell = ""
       users.each do |user|
         p user
-        if user[:password]
-          shell += "\n"
-          shell += "sudo useradd -m #{user[:login]}"
-          shell += ''
-        elsif user[:pass_file]
+        if user['password']
+          #shell += "\n"
+          #shell += "sudo useradd -m #{user[:login]}"
+          #shell += ''
+        elsif user['pass_file']
           #TODO implement pass files
+          shell += "\n"
+          shell += "sudo useradd -m #{user['login']} -s /bin/bash\n"
+          shell += "sudo mkdir -p /home/#{user['login']}/.ssh\n"
+          shell += "echo '#{user['pass_file']}' >> /home/#{user['login']}/.ssh/authorized_keys\n"
         end
       end
+      shell
     end
   end
 end
