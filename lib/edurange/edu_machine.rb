06a9c910794b0df603891a1b0b8be9d014daa92e
Stefan Boesen <stefan.boesen@gmail.com>
2013-03-04 17:17:51 -0800

    Fixed printing bug

--- lib/edurange/edu_machine.rb
+++ lib/edurange/edu_machine.rb
@@ -18,8 +18,6 @@ module Edurange
     def run(command)
       # runs an ec2 command with full path.
       command = EC2_UTILS_PATH + command
-      puts "Creating instance (ami id: #{@ami_id})"
-      puts "#{@uuid} #{@instance_id} #{@vm_size}"
       `#{command}`
     end
 
@@ -27,6 +25,7 @@ module Edurange
       # Pref user-data-file for ourselves
 
       # Create & run instance, setting instance_id and IP to match the newly created ami
+      puts "Creating instance (ami id: #{@ami_id}, size: #{@vm_size})"
       command = "ec2-run-instances #{@ami_id} -t #{@vm_size} --region us-east-1 --key #{@key_name} --user-data-file my-user-script.sh"
       self.run(command)
       @instance_id = self.get_last_instance_id()
