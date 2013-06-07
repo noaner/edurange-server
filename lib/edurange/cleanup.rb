416a45fb494eb916dd34694e9750120671d14f68
Miles Crabill <miles@milescrabill.com>
2013-06-07 13:04:17 -0700

    Changed the conditional structure of cleanup.rb

--- lib/edurange/cleanup.rb
+++ lib/edurange/cleanup.rb
@@ -18,19 +18,19 @@ end
 vpc_ids.each do |vpc_id|
 	vpc = AWS::EC2::VPCCollection.new[vpc_id]
 
-	unless vpc.instances.nil?
+	if vpc.instances
 		vpc.instances.each do |instance|
 			instance.disassociate_elastic_ip
 			instance.delete
 		end
 	end	
 
-	unless vpc.security_groups.nil?
+	if vpc.security_groups
 		vpc.security_groups.each do |security_group|
 			security_group.delete
 		end
 	
-	unless vpc.instances.nil?
+	if vpc.instances
 		vpc.instances.each do |instance|
 			if instance.elastic_ip
 				instance.disassociate_elastic_ip
@@ -40,36 +40,36 @@ vpc_ids.each do |vpc_id|
 	end
 
 
-	unless vpc.subnets.nil?
+	if vpc.subnets
 		vpc.subnets.each do |subnet|
 			subnet.delete
 		end
 	end
 
-	unless vpc.route_tables.nil?
+	if vpc.route_tables
 		vpc.route_tables.each do |route_table|
 			route_table.delete
 		end
 	end
 
-	unless vpc.network_interfaces.nil?
+	if vpc.network_interfaces
 		vpc.network_interfaces.each do |network_interface|
 			network_interface.delete
 		end
 	end
 
-	unless vpc.dhcp_options.nil?
+	if vpc.dhcp_options
 		vpc.dhcp_options.each do |dhcp_option|
 			dhcp_option.delete
 		end
 	end
 
-	unless vpc.internet_gateway.nil?
+	if vpc.internet_gateway
 		vpc.internet_gateway.delete
 		ec2.new.select { |ip| !ip.associated? }.each(&:release)
 	end
 
-	unless vpc.nil?
+	if vpc
 		vpc.delete
 	end
 end
