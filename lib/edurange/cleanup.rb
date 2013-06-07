cb33fad88c6c5df4a66e3644ed92cae6bb057173
Miles Crabill <miles@milescrabill.com>
2013-06-07 11:29:17 -0700

    Escaped Git hell, added conditionals to cleanup

--- lib/edurange/cleanup.rb
+++ lib/edurange/cleanup.rb
@@ -17,34 +17,59 @@ end
 # Heavy lifting using vpc_id to identify each vpc
 vpc_ids.each do |vpc_id|
 	vpc = AWS::EC2::VPCCollection.new[vpc_id]
+
+	unless vpc.instances.nil?
+		vpc.instances.each do |instance|
+			instance.disassociate_elastic_ip
+			instance.delete
+		end
+	end	
+
+	unless vpc.security_groups.nil?
+		vpc.security_groups.each do |security_group|
+			security_group.delete
+		end
 	
-	vpc.instances.each do |instance|
-		instance.disassociate_elastic_ip
-		instance.delete
-	end
-	
-	vpc.security_groups.each do |security_group|
-		security_group.delete
-	end
-	
-	vpc.subnets.each do |subnet|
-		subnet.delete
-	end
-	
-	vpc.route_tables.each do |route_table|
-		route_table.delete
-	end
-	
-	vpc.network_interfaces.each do |network_interface|
-		network_interface.delete
+	unless vpc.instances.nil?
+		vpc.instances.each do |instance|
+			if instance.elastic_ip
+				instance.disassociate_elastic_ip
+			end
+			instance.delete
+		end
 	end
 
-	vpc.dhcp_options.each do |dhcp_option|
-		dhcp_option.delete
+
+	unless vpc.subnets.nil?
+		vpc.subnets.each do |subnet|
+			subnet.delete
+		end
 	end
 
-	vpc.internet_gateway.delete
-	ec2.new.select { |ip| !ip.associated? }.each(&:release)
+	unless vpc.route_tables.nil?
+		vpc.route_tables.each do |route_table|
+			route_table.delete
+		end
+	end
 
-	vpc.delete
+	unless vpc.network_interfaces.nil?
+		vpc.network_interfaces.each do |network_interface|
+			network_interface.delete
+		end
+	end
+
+	unless vpc.dhcp_options.nil?
+		vpc.dhcp_options.each do |dhcp_option|
+			dhcp_option.delete
+		end
+	end
+
+	unless vpc.internet_gateway.nil?
+		vpc.internet_gateway.delete
+		ec2.new.select { |ip| !ip.associated? }.each(&:release)
+	end
+
+	unless vpc.nil?
+		vpc.delete
+	end
 end
