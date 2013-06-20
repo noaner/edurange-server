59101fd76cec0d6f8b2b89694db0e8af0ff57677
Stefan Boesen <stefan.boesen@gmail.com>
2013-06-20 12:56:17 -0700

    Rename init class to runtime.

--- lib/edurange.rb
+++ lib/edurange.rb
@@ -12,19 +12,21 @@ require 'edurange/helper'
 
 
 module Edurange
-  class Init
+  class Runtime
     # ==== Attributes
     # 
     # * +config_filename+ - Takes a YAML configuration file.
     #
     # === Usage
     #
-    #   Edurange::Init.init(yaml_file)
-    def self.init(config_filename)
+    #   Edurange::Runtime.init(yaml_file)
+    def self.start(config_filename)
 
       # Gets name of key file in use from config.yml, depends on line number 
       # keyname = IO.readlines(File.expand_path('~/.edurange/config.yml'))[0].gsub("ec2_key:", "").strip
       keyname = Settings.ec2_key
+      puts keyname
+      return
       
       # Parse the configuration file, extract list of nodes
       nodes = Edurange::Parser.parse_yaml(config_filename, keyname) # format: nodes[node_name, ami_id, users, firewall_rules, packages]
