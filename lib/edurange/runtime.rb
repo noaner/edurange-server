6fe85b7dd8f15d104dc556d785743ed7a50a5fe9
Stefan Boesen <stefan.boesen@gmail.com>
2013-06-20 13:17:29 -0700

    Oops, don't terminate immediately

--- lib/edurange/runtime.rb
+++ lib/edurange/runtime.rb
@@ -12,8 +12,6 @@ module Edurange
       # Gets name of key file in use from config.yml, depends on line number 
       # keyname = IO.readlines(File.expand_path('~/.edurange/config.yml'))[0].gsub("ec2_key:", "").strip
       keyname = Settings.ec2_key
-      puts keyname
-      return
       
       # Parse the configuration file, extract list of nodes
       nodes = Edurange::Parser.parse_yaml(config_filename, keyname) # format: nodes[node_name, ami_id, users, firewall_rules, packages]
