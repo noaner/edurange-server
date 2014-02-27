7534fb4f27f987407786000da3c5ef2aa4746650
David Weinman <david@weinman.com>
2014-02-27 11:51:01 -0800

    added debug messages to management cleanup routine
    also changed bin/edurange-cleanup back to the old cleanup script, since
    i realized the management cleanup routine plugs into ecli

--- lib/edurange/management.rb
+++ lib/edurange/management.rb
@@ -10,6 +10,7 @@ module Edurange
 
         vpc.security_groups.each do |security_group|
           unless security_group.name == "default"
+            puts "Deleting security group #{security_group}"
             security_group.delete
           end
         end
@@ -18,10 +19,12 @@ module Edurange
 
           if inst.has_elastic_ip?
             eip = inst.elastic_ip
+            puts "Disassociating Elastic IP for #{instance}"
             inst.disassociate_elastic_ip
             eip.delete
           end
 
+          puts "Deleting instance #{instance}"
           inst.delete
 
           unless inst.status == :terminated then
@@ -32,24 +35,28 @@ module Edurange
 
         if vpc.subnets
           vpc.subnets.each { |subnet|
+            puts "Deleting subnet #{subnet}" 
             subnet.delete
           }
         end
 
         if vpc.network_interfaces
           vpc.network_interfaces.each { |network_interface|
+            puts "Deleting network interface #{network_interface.id}"
             network_interface.delete
           }
         end
 
         vpc.route_tables.each do |route_table|
           unless route_table.main?
+            puts "Deleting route table #{route_table.id}"
             route_table.delete
           end
         end
 
         unless vpc.internet_gateway == nil then
           igw = vpc.internet_gateway
+          puts "Deleting internet gateway #{vpc.internet_gateway.internet_gateway_id}"
           igw.detach(vpc)
           igw.delete
         end
@@ -58,20 +65,24 @@ module Edurange
           dhcp_opt = vpc.dhcp_options
           vpc.dhcp_options = 'default'
           unless dhcp_opt.id == "default"
+            puts "Deleting dhcp options #{dhcp_opt.id} from vpc #{vpc.id}"
             dhcp_opt.delete
           end
         end
 
         unless vpc.vpn_gateway == nil then
+          puts "Deleting internet gateway #{vpc.vpn_gateway.id}"
           vpc.vpn_gateway.delete
         end
 
         vpc.network_acls.each do |network_acl|
           unless network_acl.default? then
+            puts "Deleting network acl #{network_acl.id}"
             network_acl.delete
           end
         end
 
+        puts "Deleting VPC #{vpc}"
         vpc.delete
 
       end
