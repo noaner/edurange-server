4fb0ac0ec4f0a9e98786e7e11cc3b2312ac353e9
David Weinman <david@weinman.com>
2014-04-26 06:35:44 -0700

    updated cleanup again
    this time i think it runs in *most* cases

--- lib/edurange/management.rb
+++ lib/edurange/management.rb
@@ -1,141 +1,162 @@
 module Edurange
   class Management
     def self.cleanup
+
       ec2 = AWS::EC2.new
-       
-      # get an array of virtual private clouds
       vpc_collect = ec2.vpcs
 
       vpc_collect.each do |vpc|
 
-        vpc.security_groups.each do |security_group|
-          unless security_group.name == "default"
-            puts "Deleting security group #{security_group}"
-            security_group.delete
+	vpc.security_groups.each do |security_group|
+	  unless security_group.name == "default"
+	    puts "Deleting security group #{security_group}"
+	    security_group.delete
+	    if security_group.exists?
+	      $stdout.write("Waiting for #{security_group} to terminate ")
+	      while security_group.exists?
+		$stdout.write(".. ")
+		sleep(2)
+	      end
+	      puts " OK\n"
+	    end
+	  end
+	end
 
-            while security_group.exists?
-              sleep(2)
-            end
+	vpc.instances.each do |inst|
 
-          end
-        end
+	  if inst.has_elastic_ip?
+	    eip = inst.elastic_ip
+	    puts "Disassociating Elastic IP for #{inst}"
+	    inst.disassociate_elastic_ip
+	    eip.delete
 
-        vpc.instances.each do |inst|
+	    if eip.exists?
+	      $stdout.write("Waiting for Elastic IP to terminate ")
+	      while eip.exists?
+		$stdout.write(".. ")
+		sleep(2)
+	      end
+	      puts " OK\n"
+	    end
+	  end
 
-          if inst.has_elastic_ip?
-            eip = inst.elastic_ip
-            puts "Disassociating Elastic IP for #{inst}"
-            inst.disassociate_elastic_ip
-            eip.delete
+	  puts "Deleting instance #{inst}"
+	  inst.delete
+	  if inst.exists?
+	    $stdout.write("Waiting for Instance #{inst} to terminate ")
+	    unless inst.status == :terminated then
+	      $stdout.write(".. ")
+	      sleep(2)
+	    end
+	    puts " OK\n"
+	  end
+	end
 
-            while eip.exists?
-              sleep(2)
-            end
+	if vpc.subnets
+	  vpc.subnets.each do |subnet|
+	    puts "Deleting subnet #{subnet}" 
+	    begin
+	      # this causes a lot of dependancy violation errors
+	      subnet.delete
 
-          end
+	      unless subnet.state != :pending
+		sleep(2)
+	      end
 
-          puts "Deleting instance #{inst}"
-          inst.delete
-
-          unless inst.status == :terminated then
-            sleep(2)
-          end
-
-        end
-
-        if vpc.subnets
-          vpc.subnets.each do |subnet|
-            puts "Deleting subnet #{subnet}" 
-            begin
-              subnet.delete
-
-              unless subnet.state != :pending
-                sleep(2)
-              end
-
-            rescue Exception => e
-              puts e.message
-              puts "#{subnet}'s instance statuses are as follows:"
-              subnet.instances.each { |inst|
-                puts "#{inst} status = #{inst.status}"
-              }
-              puts e.backtrace.inspect 
-            end
-          end
-        end
+	    rescue Exception => e
+	      puts e.message
+	      if subnet.instances
+		puts "#{subnet}'s instance statuses are as follows:"
+		subnet.instances.each { |inst|
+		  puts "#{inst} status = #{inst.status}"
+		}
+	      end
+	      puts "EDURange cleanup will keep going anyway."
+	    end
+	  end
+	end
 
 
-        vpc.route_tables.each do |route_table|
-          unless route_table.main?
-            puts "Deleting route table #{route_table.id}"
-            route_table.delete
-          end
-        end
+	vpc.route_tables.each do |route_table|
+	  unless route_table.main?
+	    puts "Deleting route table #{route_table.id}"
 
-        unless vpc.internet_gateway == nil then
-          igw = vpc.internet_gateway
-          puts "Deleting internet gateway #{vpc.internet_gateway.internet_gateway_id}"
-          igw.detach(vpc)
-          igw.delete
-        end
+	    if route_table.subnets 
+	      route_table.subnets.each do |subnet|
+		subnet.set_route_table(ec2.route_tables.main_route_table)
+	      end
+	    end
 
-        unless vpc.dhcp_options == nil then
-          dhcp_opt = vpc.dhcp_options
-          vpc.dhcp_options = 'default'
-          unless dhcp_opt.id == "default"
-            puts "Deleting dhcp options #{dhcp_opt.id} from vpc #{vpc.id}"
-            dhcp_opt.delete
-          end
-        end
+	    route_table.delete
+	  end
+	end
 
-        unless vpc.vpn_gateway == nil then
-          puts "Deleting internet gateway #{vpc.vpn_gateway.id}"
-          vpc.vpn_gateway.delete
-        end
+	unless vpc.internet_gateway == nil then
+	  igw = vpc.internet_gateway
+	  puts "Deleting internet gateway #{vpc.internet_gateway.internet_gateway_id}"
+	  igw.detach(vpc)
+	  igw.delete
+	end
 
-        vpc.network_acls.each do |network_acl|
-          unless network_acl.default? then
-            puts "Deleting network acl #{network_acl.id}"
-            network_acl.delete
-          end
-        end
+	unless vpc.dhcp_options == nil then
+	  dhcp_opt = vpc.dhcp_options
+	  unless dhcp_opt.id == "default"
+	    puts "Deleting dhcp options #{dhcp_opt.id} from vpc #{vpc.id}"
 
-        puts "Deleting VPC #{vpc}"
-        vpc.delete
+	    if dhcp_opt.vpcs
+	      dhcp_opt.vpcs.each { |vpc|
+		vpc.dhcp_options = 'default'
+	      }
+	    end
+
+	    dhcp_opt.delete
+	  end
+	end
+
+
+	unless vpc.vpn_gateway == nil then
+	  puts "Deleting internet gateway #{vpc.vpn_gateway.id}"
+	  vpc.vpn_gateway.delete
+	end
+
+	vpc.network_acls.each do |network_acl|
+	  unless network_acl.default? then
+	    puts "Deleting network acl #{network_acl.id}"
+	    network_acl.delete
+	  end
+	end
+
+	puts "Deleting VPC #{vpc}"
+	# this delete causes a lot of errors
+	begin
+	  vpc.delete
+
+	rescue Exception => e
+	  puts e.message
+	  puts "EDURange cleanup will try again now."
+	  self.cleanup
+	end
 
       end
 
       vol_collect = ec2.volumes
 
       vol_collect.each do |volume|
-        unless volume.status == :in_use then
-          puts "Deleting volume #{volume.id}"
-          volume.delete
-        end
+	if volume.status == :available then
+	  puts "Deleting volume #{volume.id}"
+	  volume.delete
+	end
       end
 
       elastic_ip_collect = ec2.elastic_ips
 
       elastic_ip_collect.each do |elastic_ip|
-        puts "Deleting elastic ip #{elastic_ip}"
-        elastic_ip.delete
+	puts "Deleting elastic ip #{elastic_ip}"
+	elastic_ip.delete
       end
-
+      return
     end
-  end
-end
 
-=begin
-        if vpc.network_interfaces
-          vpc.network_interfaces.each do |network_interface|
-            puts "Deleting network interface #{network_interface.id}"
-            network_interface.delete
-          end
 
-          unless network_interface.status == :terminated then
-            sleep(2)
-          end
 
-        end
-=end
 
