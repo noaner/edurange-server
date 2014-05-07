953d7899d347452ba020d4c86c3afcb4582a394c
Stefan Boesen <stefan.boesen@gmail.com>
2014-05-07 10:49:03 -0700

    Unignore settings.yml, load settings from settings.local.yml

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -1,6 +1,7 @@
 RailsConfig.setup do |config|
   config.const_name = "Settings"
   RailsConfig.load_files(
-    Rails.root.join("config", "settings.yml").to_s
+    Rails.root.join("config", "settings.yml").to_s,
+    Rails.root.join("config", "settings.local.yml").to_s
   )
 end
