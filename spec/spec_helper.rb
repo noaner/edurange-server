e6cb0723b0d375c842b73ed44183884ef56ee4e2
Stefan Boesen <stefan.boesen@gmail.com>
2014-05-16 19:12:11 -0700

    Require continuation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'plymouth'
+require 'continuation'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
