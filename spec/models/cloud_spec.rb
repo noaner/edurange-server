f6af9f91e46eb5959c7777268b7e1e494f5c9038
Stefan Boesen <stefan.boesen@gmail.com>
2014-05-17 00:10:15 -0700

    Add more tests

--- spec/models/cloud_spec.rb
+++ spec/models/cloud_spec.rb
@@ -37,4 +37,14 @@ describe Cloud do
     @cloud.valid?
     expect(@cloud).to have(1).errors_on(:cidr_block)
   end
+  it 'should not be valid with a very small network' do
+    @cloud.cidr_block = '10.0.0.0/29'
+    @cloud.valid?
+    expect(@cloud).to have(1).errors_on(:cidr_block)
+  end
+  it 'should not be valid with a very large network' do
+    @cloud.cidr_block = '10.0.0.0/15'
+    @cloud.valid?
+    expect(@cloud).to have(1).errors_on(:cidr_block)
+  end
 end
