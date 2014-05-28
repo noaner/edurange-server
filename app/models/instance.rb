538a0472993c2879328e89a7f464002590658b2e
Miles Crabill <miles@milescrabill.com>
2014-05-28 10:22:50 -0700

    fixed calls to public_ip to use method_missing, added current dir start/stop scripts

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -12,10 +12,6 @@ class Instance < ActiveRecord::Base
   
   before_create :ensure_has_ip
   validate :ip_address_must_be_within_subnet
-  
-  def public_ip
-    "my ip! :)"
-  end
 
   def ensure_has_ip
     if self.ip_address.blank?
