fe710ac7ec1fcd35aa4dc7299dda50e6f0b7b164
yandil <topiary@openmailbox.org>
2015-07-30 15:28:48 -0700

    add methods for populating statistic with bash histories and doing some analytics

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -3,9 +3,23 @@ class Statistic < ActiveRecord::Base
 	has_one :scenario
     
     attr_accessor :bash_histories  # like @bash_histories
-    
-    after_initialize do |statistic|
-    	puts "Statistic has been instantiated."
+   
+    def populate_bash_histories(scenario)
+    	scenario.instances.all.each do |intsance|
+    		self.bash_histories += instance.get_bash_history
+    	end
+    end
+
+    def bash_analytics(relevant_commands)
+    	options_frequencies = Hash.new(0)
+    	bash_history = self.bash_histories.split("\n")
+    	bash_history.each do |command|
+    		options = command.scan(/[-'[A-Za-z]]+/);
+    		options.each do |option|
+    			options_frequencies[option] += 1;
+    		end
+    	end	
+    	options_frequencies.sort_by { |option| option[1] }
     end
 
 end
