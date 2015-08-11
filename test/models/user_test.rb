376ff23bc5386fc461f7e613a1a56abbd82d8481
kahea <kaheah@gmail.com>
2015-08-11 12:19:51 -1000

    change indentation from tabs to spaces is user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,249 +2,249 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-	test 'should not save user without email, name, & password' do
-		user = User.new
-		user.save
-		assert_not user.valid?
-		assert_equal [:email, :password, :name], user.errors.keys
-	end
+  test 'should not save user without email, name, & password' do
+    user = User.new
+    user.save
+    assert_not user.valid?
+    assert_equal [:email, :password, :name], user.errors.keys
+  end
 
-	test 'should not save user with unallowed name' do
-		user = users(:test1)
+  test 'should not save user with unallowed name' do
+    user = users(:test1)
 
-		user.name = ""
-		user.save
-		assert_not user.valid?
-		assert_equal [:name], user.errors.keys
+    user.name = ""
+    user.save
+    assert_not user.valid?
+    assert_equal [:name], user.errors.keys
 
-		user.name = "   "
-		user.save
-		assert_not user.valid?
-		assert_equal [:name], user.errors.keys
+    user.name = "   "
+    user.save
+    assert_not user.valid?
+    assert_equal [:name], user.errors.keys
 
-		user.name = "abs$$$$"
-		user.save
-		assert_not user.valid?
-		assert_equal [:name], user.errors.keys
+    user.name = "abs$$$$"
+    user.save
+    assert_not user.valid?
+    assert_equal [:name], user.errors.keys
 
-		user.name = "____"
-		user.save
-		assert_not user.valid?
-		assert_equal [:name], user.errors.keys
+    user.name = "____"
+    user.save
+    assert_not user.valid?
+    assert_equal [:name], user.errors.keys
 
-		user.name = "foo_bar"
-		user.save
-		assert user.valid?
-		assert_equal [], user.errors.keys
-	end
+    user.name = "foo_bar"
+    user.save
+    assert user.valid?
+    assert_equal [], user.errors.keys
+  end
 
-	test 'should have default student role' do
-		user = User.new(name: "foo", email: "foo@edurange.org", password: "nothing8")
-		user.save
-		assert user.valid?
-		assert user.role == "student"
-	end
+  test 'should have default student role' do
+    user = User.new(name: "foo", email: "foo@edurange.org", password: "nothing8")
+    user.save
+    assert user.valid?
+    assert user.role == "student"
+  end
 
-	test 'should not allow name update while scenario is running' do
-		user = users(:test1)
+  test 'should not allow name update while scenario is running' do
+    user = users(:test1)
 
-		scenario = user.scenarios.new(name: 'testscenario')
-		scenario.save
+    scenario = user.scenarios.new(name: 'testscenario')
+    scenario.save
 
-		assert scenario.valid?
+    assert scenario.valid?
 
-		scenario.set_booted
-		user.name = "testchange"
-		user.save
-		assert_equal [:running], user.errors.keys
+    scenario.set_booted
+    user.name = "testchange"
+    user.save
+    assert_equal [:running], user.errors.keys
 
-		scenario.set_stopped
-		user.name = "testchange"
-		user.save
-		assert_equal [], user.errors.keys
+    scenario.set_stopped
+    user.name = "testchange"
+    user.save
+    assert_equal [], user.errors.keys
 
-		scenario.set_booting
-		user.name = "testchange"
-		user.save
-		assert_equal [:running], user.errors.keys
+    scenario.set_booting
+    user.name = "testchange"
+    user.save
+    assert_equal [:running], user.errors.keys
 
-		[:paused, :pausing, :starting, :queued_boot, 
-			:queued_unboot, :booting, :booted, :failure, :boot_failed, 
-			:unboot_failed, :unbooting, :stopping, :partially_booted, 
-			:partially_booted_with_failure, :partially_unbooted, 
-			:partially_unbooted_with_failure].each do |status|
-				scenario.status = status
-				scenario.save
+    [:paused, :pausing, :starting, :queued_boot, 
+      :queued_unboot, :booting, :booted, :failure, :boot_failed, 
+      :unboot_failed, :unbooting, :stopping, :partially_booted, 
+      :partially_booted_with_failure, :partially_unbooted, 
+      :partially_unbooted_with_failure].each do |status|
+        scenario.status = status
+        scenario.save
 
-				user.name = "testchange"
-				user.save
-				assert_not user.valid?
-				assert_equal [:running], user.errors.keys
-		end
-	end
+        user.name = "testchange"
+        user.save
+        assert_not user.valid?
+        assert_equal [:running], user.errors.keys
+    end
+  end
 
-	test 'should email credentials' do 
-		user = users(:test1)
-		email = user.email_credentials('thisisnewpass')
-		assert_not ActionMailer::Base.deliveries.empty?
-	end
+  test 'should email credentials' do 
+    user = users(:test1)
+    email = user.email_credentials('thisisnewpass')
+    assert_not ActionMailer::Base.deliveries.empty?
+  end
 
-	test 'should set correct roles' do 
-		user = users(:test1)
-		user.set_student_role
-		assert_match("student", user.role)
-		assert user.is_student?
-		assert_not user.is_instructor?
-		assert_not user.is_admin?
+  test 'should set correct roles' do 
+    user = users(:test1)
+    user.set_student_role
+    assert_match("student", user.role)
+    assert user.is_student?
+    assert_not user.is_instructor?
+    assert_not user.is_admin?
 
-		user.set_instructor_role
-		assert_match("instructor", user.role)
-		assert_not user.is_student?
-		assert user.is_instructor?
-		assert_not user.is_admin?
+    user.set_instructor_role
+    assert_match("instructor", user.role)
+    assert_not user.is_student?
+    assert user.is_instructor?
+    assert_not user.is_admin?
 
-		user.set_admin_role
-		assert_match("admin", user.role)
-		assert_not user.is_student?
-		assert_not user.is_instructor?
-		assert user.is_admin?
-	end
+    user.set_admin_role
+    assert_match("admin", user.role)
+    assert_not user.is_student?
+    assert_not user.is_instructor?
+    assert user.is_admin?
+  end
 
-	test 'should have student group user if instructor or admin' do
-		user = users(:test1)
-		student = users(:student1)
-		scenario = scenarios(:test1)
+  test 'should have student group user if instructor or admin' do
+    user = users(:test1)
+    student = users(:student1)
+    scenario = scenarios(:test1)
 
-		# test instructor role
-		user.set_instructor_role
+    # test instructor role
+    user.set_instructor_role
 
-		# make sure the user has student group named All
-		assert user.student_groups.size == 1
-		allsg = user.student_groups.find_by_name("All")
-		assert allsg.valid?
+    # make sure the user has student group named All
+    assert user.student_groups.size == 1
+    allsg = user.student_groups.find_by_name("All")
+    assert allsg.valid?
 
-		sgu = allsg.student_group_users.new(user_id: student.id)
-		sgu.save
-		assert sgu.valid?
+    sgu = allsg.student_group_users.new(user_id: student.id)
+    sgu.save
+    assert sgu.valid?
 
-		assert allsg.student_group_users.size == 1
-		assert allsg.student_group_users.find_by_user_id(student.id).valid?
-		
-		# make sure user can not become student with a scenario that is running
-		scenario.user_id = user.id
-		scenario.save
-		assert scenario.valid?
-		assert scenario.user_id = user.id
+    assert allsg.student_group_users.size == 1
+    assert allsg.student_group_users.find_by_user_id(student.id).valid?
+    
+    # make sure user can not become student with a scenario that is running
+    scenario.user_id = user.id
+    scenario.save
+    assert scenario.valid?
+    assert scenario.user_id = user.id
 
-		user.reload
-		assert user.scenarios.size == 1
+    user.reload
+    assert user.scenarios.size == 1
 
-		assert user.validate_running
-		scenario.set_booted
+    assert user.validate_running
+    scenario.set_booted
 
-		user.reload
+    user.reload
 
-		user.set_student_role
-		assert_equal [:running], user.errors.keys
-		user.errors.clear
+    user.set_student_role
+    assert_equal [:running], user.errors.keys
+    user.errors.clear
 
-		scenario.set_stopped
-		scenario.reload
-		user.reload
+    scenario.set_stopped
+    scenario.reload
+    user.reload
 
-		# make sure when user becomes student they have no student groups or student group users
-		user.set_student_role
-		assert_equal [], user.errors.keys
+    # make sure when user becomes student they have no student groups or student group users
+    user.set_student_role
+    assert_equal [], user.errors.keys
 
-		assert_not user.student_groups.any?
-		assert_not user.student_group_users.any?
+    assert_not user.student_groups.any?
+    assert_not user.student_group_users.any?
 
-		# Do the same tests for admin
-		user.reload
-		scenario.reload
-		user.set_admin_role
+    # Do the same tests for admin
+    user.reload
+    scenario.reload
+    user.set_admin_role
 
-		# make sure the user has student group named All
-		assert user.student_groups.size > 0
-		allsg = user.student_groups.find_by_name("All")
-		allsg.save
-		assert allsg.valid?
+    # make sure the user has student group named All
+    assert user.student_groups.size > 0
+    allsg = user.student_groups.find_by_name("All")
+    allsg.save
+    assert allsg.valid?
 
-		sgu = allsg.student_group_users.new(user_id: student.id)
-		sgu.save
-		assert sgu.valid?
+    sgu = allsg.student_group_users.new(user_id: student.id)
+    sgu.save
+    assert sgu.valid?
 
-		assert allsg.student_group_users.size == 1
-		assert allsg.student_group_users.find_by_user_id(student.id).valid?
-		
-		# make sure user can not become student with a scenario that is running
-		scenario.user_id = user.id
-		scenario.save
-		assert scenario.valid?
-		assert scenario.user_id = user.id
+    assert allsg.student_group_users.size == 1
+    assert allsg.student_group_users.find_by_user_id(student.id).valid?
+    
+    # make sure user can not become student with a scenario that is running
+    scenario.user_id = user.id
+    scenario.save
+    assert scenario.valid?
+    assert scenario.user_id = user.id
 
-		user.reload
-		assert user.scenarios.size == 1
+    user.reload
+    assert user.scenarios.size == 1
 
-		assert user.validate_running
-		scenario.set_booted
+    assert user.validate_running
+    scenario.set_booted
 
-		user.reload
+    user.reload
 
-		user.set_student_role
-		assert_equal [:running], user.errors.keys
-		user.errors.clear
+    user.set_student_role
+    assert_equal [:running], user.errors.keys
+    user.errors.clear
 
-		scenario.set_stopped
-		scenario.reload
-		user.reload
+    scenario.set_stopped
+    scenario.reload
+    user.reload
 
-		# make sure when user becomes student they have no student groups or student group users
-		user.set_student_role
-		assert_equal [], user.errors.keys
+    # make sure when user becomes student they have no student groups or student group users
+    user.set_student_role
+    assert_equal [], user.errors.keys
 
-		assert_not user.student_groups.any?
-		assert_not user.student_group_users.any?
-	end
+    assert_not user.student_groups.any?
+    assert_not user.student_group_users.any?
+  end
 
-	test 'should own all resources belonging to scenario and student groups' do
-		user = users(:test1)
-		user.set_admin_role
+  test 'should own all resources belonging to scenario and student groups' do
+    user = users(:test1)
+    user.set_admin_role
 
-		# test ownership of student groups
-		sg = user.student_groups.new(name: 'test')
-		sg.save
-		assert sg.valid?
+    # test ownership of student groups
+    sg = user.student_groups.new(name: 'test')
+    sg.save
+    assert sg.valid?
 
-		user.student_groups.each do |sg|
-			assert user.owns? sg
-		end
+    user.student_groups.each do |sg|
+      assert user.owns? sg
+    end
 
-		# test ownership of scenario and its resources
-		scenario = YmlRecord.load_yml('strace', user)
-		assert user.owns? scenario
+    # test ownership of scenario and its resources
+    scenario = YmlRecord.load_yml('strace', user)
+    assert user.owns? scenario
 
-		scenario.clouds.each do |cloud|
-			assert user.owns? cloud
-			
-			cloud.subnets.each do |subnet|
-				assert user.owns? subnet
+    scenario.clouds.each do |cloud|
+      assert user.owns? cloud
+      
+      cloud.subnets.each do |subnet|
+        assert user.owns? subnet
 
-				subnet.instances.each do |instance|
-					assert user.owns? instance
-					instance.instance_roles.each { |ir| assert user.owns? ir } 
-					instance.instance_groups.each { |ig| assert user.owns? ig }
+        subnet.instances.each do |instance|
+          assert user.owns? instance
+          instance.instance_roles.each { |ir| assert user.owns? ir } 
+          instance.instance_groups.each { |ig| assert user.owns? ig }
 
-					instance.roles.each do |role|
-						assert user.owns? role
-						role.role_recipes.each { |rr| assert user.owns? rr }
+          instance.roles.each do |role|
+            assert user.owns? role
+            role.role_recipes.each { |rr| assert user.owns? rr }
 
-					end
-				end
-			end 
-		end
-		scenario.recipes.each { |r| assert user.owns? r }
-	end
+          end
+        end
+      end 
+    end
+    scenario.recipes.each { |r| assert user.owns? r }
+  end
 
 
 end
