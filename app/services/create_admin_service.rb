062592c4315fb225364097cb1df05bf03384d607
kahea <kaheah@gmail.com>
2015-08-19 08:18:40 -1000

    update create admin service

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,9 +1,12 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-        user.admin!
-      end
+  	user = nil
+  	if not user = User.find_by_email(Rails.application.secrets.admin_email)
+  		user = User.new(email: Rails.application.secrets.admin_email, name: Rails.application.secrets.admin_name)
+  	end
+  	user.password = Rails.application.secrets.admin_password
+  	user.password_confirmation = Rails.application.secrets.admin_password
+  	user.set_admin_role
+    user
   end
 end
