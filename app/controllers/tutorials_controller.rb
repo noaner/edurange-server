0deedae472b85b7ae946168a623aa9c6a8184946
UserJeff <jeff.ladish@gmail.com>
2016-01-07 14:26:56 -0800

    added set_user method to enable user-speficic docs

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,4 +1,7 @@
 class TutorialsController < ApplicationController
+  before_action :authenticate_admin_or_instructor
+  before_action :set_user
+
   def index
       #@tutorials = Tutorial.all
   end
@@ -13,4 +16,12 @@ class TutorialsController < ApplicationController
   def student_manual
     send_file("#{Rails.root}/app/views/tutorials/EDURange_for_students.pdf", :disposition => 'inline',:type => 'application/pdf')
   end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(current_user.id)
+    end
+
 end
