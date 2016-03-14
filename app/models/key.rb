73648caca7439e995b66744a0af0fec0fd058da3
thebareju <noahbweiner@gmail.com>
2016-03-14 11:31:24 -0700

    Add bitfield flags to key and key chain

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,4 +1,14 @@
 class Key < ActiveRecord::Base
+  include FlagShihTzu
+
   belongs_to :resource, polymorphic: true
   belongs_to :key_chain
+
+  validates :key_chain, presence: true
+  validates :resource, presence: true
+
+  # boolean bitfield provided by flag_shih_tzu gem
+  has_flags 1 => :can_view,
+            2 => :can_edit,
+            3 => :can_destroy
 end
--- app/models/key_chain.rb
+++ app/models/key_chain.rb
@@ -1,4 +1,10 @@
 class KeyChain < ActiveRecord::Base
-    has_many :keys
-    has_and_belongs_to_many :users
+  include FlagShihTzu
+
+  has_many :keys
+  has_and_belongs_to_many :users
+
+  # boolean bitfield provided by flag_shih_tzu gem
+  has_flags 1 => :can_create_user,
+            2 => :can_create_scenario
 end
