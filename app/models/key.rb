188d197fae6ed9c1fc3a9712ee9e6c1c4bb74b2c
thebareju <noahbweiner@gmail.com>
2016-04-29 17:34:31 -0700

    Refactor key code

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,4 +1,5 @@
 class Key < ActiveRecord::Base
+  # boolean bitfield provided by flag_shih_tzu gem
   include FlagShihTzu
 
   belongs_to :resource, polymorphic: true
@@ -10,13 +11,16 @@ class Key < ActiveRecord::Base
   DEFAULT_FLAGS = [:edit, :view, :destroy]
   FLAG_LIMIT = 16
 
-  # boolean bitfield provided by flag_shih_tzu gem
-  # default false
+  # This basically creates the properties flag_1 through flag_16 for the model.
+  # If I were less lazy I'd hard code the bitwise logic, since FlagShihTzu
+  # wasn't made for this and adds a lot of cruft by defining functions for each
+  # flag.
   (1..16).each do |i|
     has_flags i => "flag_#{i}".to_sym
   end
 
   def flags_for(obj)
+    # return a list of flags for an object
     if defined? obj.class::FLAGS
       obj.class::FLAGS
     else
@@ -25,32 +29,35 @@ class Key < ActiveRecord::Base
   end
 
   def flag_name(flag)
-    "flag_#{flags_for(self.resource).index(flag) + 1}"
+    # map named flag (e.g. :edit) to a numbered flag (e.g. flag_2)
+    index = flags_for(self.resource).index(flag) + 1
+    raise OutOfFlagColumns if index > FLAG_LIMIT
+    "flag_#{index}"
   end
 
-  # test whether a flag is set
   def can?(flag)
+    # test whether a flag is set
     self.send flag_name(flag)
   end
 
-  # set a flag or flags to true
   def can!(*flags)
+    # set a flag or flags to true
     flags.each do |flag|
       self.send "#{flag_name(flag)}=", true
     end
     self.save
   end
 
-  # set a flag or flags to false
   def cannot!(*flags)
+    # set a flag or flags to false
     flags.each do |flag|
       self.send "#{flag_name(flag)}=", false
     end
     self.save
   end
 
-  # set all flags to a value
   def set_all_flags(value)
+    # set all flags to a value
     flags_for(self.resource).each do |flag|
       self.send "#{flag_name(flag)}=", value
     end
